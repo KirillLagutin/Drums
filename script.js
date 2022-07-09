36aa1a09b983b1fd8e1fa310261cd7d8b3051038
Kirill Lagutin <lagutin.k@mail.ru>
2022-07-10 00:31:08 +0300

    Add mouse click

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
-document.addEventListener('keydown', function(e) {
-    let key = e.key;
-    let drum = document.querySelector(`[data-d=${key}]`);
-    let audio = document.querySelector(`[data-a=${key}]`);
+let keyKeyboard = document.addEventListener('keydown', playNote);
 
-    drum.classList.add('hit');
+let keys = document.querySelectorAll('.drum');
+keys.forEach(key => {
+    key.addEventListener('click', playNote);
+});
+
+function playNote(e) {
+    let key, audio;
+    
+    if(e.keyCode) {
+        key = document.querySelector(`[data-d="${e.keyCode}"]`);
+        audio = document.querySelector(`[data-a="${e.keyCode}"]`);
+    }
+    else {
+        key = e.target;
+        audio = document.querySelector(`[data-a="${key.dataset.d}"]`);
+    }
+
+    key.classList.add('hit')
     setTimeout(()=>{
-        drum.classList.remove('hit');
+        key.classList.remove('hit')
     }, 100);
 
     audio.currentTime = 0;
     audio.play();
-});
+}
